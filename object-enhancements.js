0d25c2cf7c49435a62c17c11d5c9aeb099fce3dc
Mark Mazurkiewicz - C <mmazurkiewicz@blueorigin.com>
2022-12-12 12:21:16 -0800

    exercise progress made

---
+++ object-enhancements.js
@@ -0,0 +1,64 @@
+function createInstructor(firstName, lastName){
+    return {
+      firstName: firstName,
+      lastName: lastName
+    }
+  }
+
+/* Write an ES2015 Version */
+
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+
+/* --------------------------------------------------------- */
+
+var favoriteNumber = 42;
+var instructor = {
+  firstName: "Colt"
+}
+instructor[favoriteNumber] = "That is my favorite!"
+
+/* Write an ES2015 Version */
+const favoriteNumber = 42;
+const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+}
+
+/* --------------------------------------------------------- */
+
+var instructor = {
+    firstName: "Colt",
+    sayHi: function(){
+      return "Hi!";
+    },
+    sayBye: function(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+/* Write an ES2015 Version */
+
+
+
+
+
+
+
+/* --------------------------------------------------------- */
+
+function createAnimal(species, verb, noise) {
+
+}
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
--- rest-spread.js
+++ rest-spread.js
@@ -70,19 +70,21 @@ function addKeyVal(obj, key, val) {
 /** Return a new object with a key removed. */
 
 function removeKey(obj, key) {
-        
+    const newObj = {...obj, };
+    delete newObj[key];
+    return newObj;
 }
-
 
 /** Combine two objects and return a new object. */
 
 function combine(obj1, obj2) {
-
+    return {...obj1, ...obj2 };
 }
 
 
 /** Return a new object with a modified key and value. */
 
 function update(obj, key, val) {
+    return {...obj, [key]: val}
 
 }
